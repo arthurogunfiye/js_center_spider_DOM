ed81be1695d574d3ef58f0e7ec28691ae0abf4ba
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-30 06:26:03 +0100

    make build

---
+++ index.84f73b3d.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");t.style.width="30px",t.style.position="relative",t.style.top="".concat(.5*t.offsetParent.clientHeight-.5*t.clientHeight,"px"),t.style.left="".concat(.5*t.offsetParent.offsetWidth-.5*t.offsetWidth,"px");
+//# sourceMappingURL=index.84f73b3d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const spider = document.querySelector(".spider");
+spider.style.width = "30px";
+spider.style.position = "relative";
+spider.style.top = `${spider.offsetParent.clientHeight * 0.5 - spider.clientHeight * 0.5}px`;
+spider.style.left = `${spider.offsetParent.offsetWidth * 0.5 - spider.offsetWidth * 0.5}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
